99b0aa98feca765b8c00cbeb3ce2fe01f6bb96fb
AleksDu <aleksandr.dyshkin@gmail.com>
2021-08-23 19:28:00 +0300

    Update rendering.js

--- src/js/rendering.js
+++ src/js/rendering.js
@@ -16,7 +16,11 @@ function fetchCountries(searchQuery) {
     const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`; 
 
     return fetch(url)
-    .then(response =>  response.json())     
+    .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+      })     
 };
 
 refs.input.addEventListener(
